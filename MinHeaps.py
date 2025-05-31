e04d500b41fb05bcfcd899cb98c5179b8d036c1e
Edwin Molina <edwin.molina01@unicatolica.edu.co>
2025-05-30 22:10:32 -0500

    Update MinHeaps.py
    Cree la clase MinHeap la cual tiene su estructura base  con índices, verificación de hojas e intercambio"

--- MinHeaps.py
+++ MinHeaps.py
@@ -8,3 +8,24 @@ class Tarea:
 
     def _str_(self):
         return f"{self.nombre} (Prioridad: {self.prioridad})"
+
+class MinHeap:
+    def __init__(self, max_size):
+        self.heap = [None] * max_size
+        self.size = 0
+        self.max_size = max_size
+
+    def parent_index(self, i):
+        return (i - 1) // 2
+
+    def left_child_index(self, i):
+        return 2 * i + 1
+
+    def right_child_index(self, i):
+        return 2 * i + 2
+
+    def is_leaf(self, i):
+        return self.left_child_index(i) >= self.size
+
+    def swap(self, i, j):
+        self.heap[i], self.heap[j] = self.heap[j], self.heap[i]
